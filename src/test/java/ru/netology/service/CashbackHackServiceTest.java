501d8a4a832dfca90c8cb48b2643184b516963e6
Sergey Sakhanskiy <ssf808867@gmail.com>
2023-03-29 12:09:23 +0300

    Dependencies deleted & Test code partly destroyed

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,11 +1,9 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 
 class CashbackHackServiceTest {
 
-    @Test
+
     void shouldSuggest200() {
         // 3 steps to create a test:
         // prepare data, target action done, comparison actual & expected
@@ -15,9 +13,6 @@ class CashbackHackServiceTest {
 
         int actual = service.remain(amount);
         int expected = 200;
-
-        Assertions.assertEquals(expected, actual);
-
 
 
     }
