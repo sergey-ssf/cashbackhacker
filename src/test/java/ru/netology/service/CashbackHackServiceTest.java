712d776ac9a087add869201f09a7e63b603cb352
Sergey Sakhanskiy <ssf808867@gmail.com>
2023-03-30 09:47:59 +0300

    TestNG tests written and work

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,22 +1,89 @@
 package ru.netology.service;
 
 
-class CashbackHackServiceTest {
+import org.testng.annotations.Test;
+import static org.testng.AssertJUnit.assertEquals;
 
+public class CashbackHackServiceTest {
 
-    void shouldSuggest200() {
+    // Проверка функциональности в интервале до 1000 рублей
+    @Test
+    public void shouldSuggest200() {
         // 3 steps to create a test:
         // prepare data, target action done, comparison actual & expected
 
         CashbackHackService service = new CashbackHackService();
-        int amount =800;
+        int amount = 800;
 
         int actual = service.remain(amount);
         int expected = 200;
 
+        assertEquals(expected, actual);
+
+    }
+
+    // Проверка функциональности, если оплата 0 рублей
+    @Test
+    public void shouldSuggest1000() {
+        // 3 steps to create a test:
+        // prepare data, target action done, comparison actual & expected
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+
+        int actual = service.remain(amount);
+        int expected = 1000;
+
+        assertEquals(expected, actual);
 
     }
 
 
+    // Проверка функциональности, если оплата 1 рубль
+    @Test
+    public void shouldSuggest999() {
+        // 3 steps to create a test:
+        // prepare data, target action done, comparison actual & expected
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1;
+
+        int actual = service.remain(amount);
+        int expected = 999;
+
+        assertEquals(expected, actual);
+    }
+
+    // Проверка функциональности, если оплата 999 рублей
+    @Test
+    public void shouldSuggest1() {
+        // 3 steps to create a test:
+        // prepare data, target action done, comparison actual & expected
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+
+        int actual = service.remain(amount);
+        int expected = 1;
+
+        assertEquals(expected, actual);
+
+    }
+
+    // Проверка функциональности, если оплата 1000 рублей
+    @Test
+    public void shouldSuggest0() {
+        // 3 steps to create a test:
+        // prepare data, target action done, comparison actual & expected
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+
+    }
 
 }
